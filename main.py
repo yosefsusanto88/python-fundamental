6780151eb74656841aa811398de8cde9a74328b2
yosefsusanto88 <gwenilona12@gmail.com>
2022-09-22 20:00:58 +0700

    test 2

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-print('ibu memberi perintah anak membeli telur')
-print('Anak menjawab OK')
+# print('Ibu berkata,"Pergi ke toko"')
+# print('Anak menjawab, "Baiklah , apa yang harus saya lakukan ditoko?"')
+# print('Ibu menjawab, "Beli satu botol susu, dan jika ada telur beli 6"')
+# print('Maka anak pergi ke toko')
+# print("Dan mulai berbelanja")
+
+jumlah_botol_susu = 50
+if jumlah_botol_susu > 0:
+    print('Anak mengecek harga, dan teryata uangnya cukup')
+    print('Anak membeli satu botol susu')
+    print('Anak bertanya,"Apakah ada telur?')
+    jumlah_telur = 0
+    if jumlah_telur > 0:
+        print('Anak membeli enam telur juga')
+        print('Anak melakukan pembayaran')
+    else:
+        print('Anak hanya membeli satu botol susu')
+else:
+    print('Anak tidak jadi membeli botol susu')
+
+print('Anak pulang ke rumah')
+print('Menyampaikan hasil kepada ibu')
+
+
+
+
+
 
