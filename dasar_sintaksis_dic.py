a2809d65b7b2e1c878609333ce3a65d442e44792
yosefsusanto88 <gwenilona12@gmail.com>
2023-06-24 20:41:25 +0700

    dasar sintaks

---
+++ dasar_sintaksis_dic.py
@@ -0,0 +1,23 @@
+users = {
+    "id": 1,
+    "name" : "Leanne Graham",
+    "username" : "Bret",
+    "email" : "Sincere@april.biz",
+    "address" : {
+        "street" : "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
+
+}
+print(users)
+print(users['id'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
+print(users['address']['geo'])
